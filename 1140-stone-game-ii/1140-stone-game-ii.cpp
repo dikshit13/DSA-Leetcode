15a346e7ba0845a29501e3a57df443e2f9aece54
Dikshit kamboj <152528727+dikshit13@users.noreply.github.com>
2026-08-09 16:34:26 +0530

    Time: 3 ms (95.69%), Space: 11.8 MB (72.97%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
+    int n;
+    vector<int> suffix;
+    int dp[105][105];
+
+    int solve(int i, int M) {
+        if (i >= n)
+            return 0;
+
+        if (dp[i][M] != -1)
+            return dp[i][M];
+
+        int best = 0;
+
+        for (int x = 1; x <= 2 * M && i + x <= n; x++) {
+            int opponent = solve(i + x, max(M, x));
+
+            int current = suffix[i] - opponent;
+
+            best = max(best, current);
+        }
+
+        return dp[i][M] = best;
+    }
+
     int stoneGameII(vector<int>& piles) {
-        
+        n = piles.size();
+
+        suffix.resize(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 1);
     }
 };
